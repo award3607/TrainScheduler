18ce9dcac71f191f3a1eefd429aeb03cd041bdb3
Andrew Ward <andrew.rampant@gmail.com>
2017-10-28 09:05:14 -0500

    saturday am progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+var config = {
+	apiKey: "AIzaSyDNRPB9UfXd9kaHq42GlnNrh7Eu58D2XWg",
+	authDomain: "train-scheduler-af06a.firebaseapp.com",
+	databaseURL: "https://train-scheduler-af06a.firebaseio.com",
+	projectId: "train-scheduler-af06a",
+	storageBucket: "",
+	messagingSenderId: "105699376771"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit-button").on("click", function() {
+	var trainName = $("#train-name").val().trim();
+	var trainDestination = $("#train-destination").val().trim();
+	var trainFirstTime = $("#train-first-time").val().trim();
+	var trainFrequency = $("#train-frequency").val().trim();
+	console.log(trainName, trainDestination, trainFirstTime, trainFrequency);
+	if(trainName && trainDestination && trainFirstTime && trainFrequency) {
+		database.ref().push({
+			trainName: trainName,
+			trainDestination: trainDestination,
+			trainFirstTime: trainFirstTime,
+			trainFrequency: trainFrequency
+		});
+	}
+	else {
+		console.log("Missing field");
+	}
+});
+
+// database.ref().once("value", function(snapshot) {
+// 	snapshot.forEach(function(childSnapshot) {
+// 		console.log(childSnapshot.val());
+// 		// $("tbody").empty();
+// 		displayTrain(childSnapshot.val());
+// 	});
+// 	}, function(errorObject) {
+// 	console.log("The read failed: " + errorObject.code);
+// });
+
+database.ref().on("child_added", function(snapshot) {
+	displayTrain(snapshot.val());
+	}, function(errorObject) {
+	console.log("The read failed: " + errorObject.code);
+});
+
+function displayTrain(data) {
+	var arrivalData = getArrivalData(data.trainFirstTime, data.trainFrequency);
+	var $row = $("<tr>");
+	$row.append($("<td>").text(data.trainName))
+		.append($("<td>").text(data.trainDestination))
+		.append($("<td>").text(data.trainFrequency))
+		.append($("<td>").text(arrivalData.nextTime))
+		.append($("<td>").text(arrivalData.minutesAway));
+	$("tbody").append($row);
+}
+
+function getArrivalData(firstTime, frequency) {
+	var data = {};
+	var arrival = new Date();
+	arrival.setHours(firstTime.split(":")[0], firstTime.split(":")[1]);
+	var now = new Date();
+
+	//if arrival is in the past, add frequency to arrival until the result is in the future
+	while (arrival.getTime() < now.getTime()) {
+		arrival.setTime(arrival.getTime() + (frequency * 60 * 1000));
+	}
+		
+	//minutesAway is the difference between arrival and now
+	data.minutesAway = (arrival.getTime() - now.getTime()) / 60 / 1000;
+	var arrivalHour = arrival.getHours().toString();
+	var arrivalMinute;
+	if (arrival.getMinutes() < 10) {
+		arrivalMinute = 
+	}
+	var arrivalMinute = arrival.getMinutes().toString();
+	data.nextTime = arrival.getHours().toString() + ":" + arrival.getMinutes().toString();
+
+	return data;
+}
+
+function padMinuteString(s) {
+	if(s.length > 1) {
+		s.
+	}
+}
